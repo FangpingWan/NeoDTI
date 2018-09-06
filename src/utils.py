99e9c0e702edc36897f001e938dc6b049d1fbf92
FangpingWan <wanfangping92@gmail.com>
2018-09-06 15:23:39 +0800

    Update utils.py
    L2 regularization added.

--- src/utils.py
+++ src/utils.py
@@ -15,6 +15,7 @@ def bias_variable(shape):
 def a_layer(x,units):
     W = weight_variable([x.get_shape().as_list()[1],units])
     b = bias_variable([units])
+    tf.add_to_collection('l2_reg', tf.contrib.layers.l2_regularizer(1.0)(W))
     return relu(tf.matmul(x, W) + b)
 
 
@@ -22,9 +23,12 @@ def bi_layer(x0,x1,sym,dim_pred):
     if sym == False:
         W0p = weight_variable([x0.get_shape().as_list()[1],dim_pred])
         W1p = weight_variable([x1.get_shape().as_list()[1],dim_pred])
+        tf.add_to_collection('l2_reg', tf.contrib.layers.l2_regularizer(1.0)(W0p))
+        tf.add_to_collection('l2_reg', tf.contrib.layers.l2_regularizer(1.0)(W1p))
         return tf.matmul(tf.matmul(x0, W0p), 
                             tf.matmul(x1, W1p),transpose_b=True)
     else:
         W0p = weight_variable([x0.get_shape().as_list()[1],dim_pred])
+        tf.add_to_collection('l2_reg', tf.contrib.layers.l2_regularizer(1.0)(W0p))
         return tf.matmul(tf.matmul(x0, W0p), 
                             tf.matmul(x1, W0p),transpose_b=True)
